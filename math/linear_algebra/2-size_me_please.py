ed6da819c8b5eefc1f25c162f4d96e53aa4ff648
khalilBahi <7753@holbertonstudents.com>
2024-12-03 13:59:02 +0100

    calculates the shape of a matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,14 +5,14 @@ def matrix_shape(matrix):
     Calculate the shape of a matrix.
 
     The function determines the dimensions of a given matrix (nested lists),
-    returning its shape as a list of integers. Each integer in the list 
+    returning its shape as a list of integers. Each integer in the list
     represents the size of the matrix at a specific level of nesting.
 
     Args:
         matrix (list): A nested list representing the matrix.
 
     Returns:
-        list: A list of integers where each value represents the size of the 
+        list: A list of integers where each value represents the size of the
               corresponding dimension of the matrix.
     """
     shape = []
