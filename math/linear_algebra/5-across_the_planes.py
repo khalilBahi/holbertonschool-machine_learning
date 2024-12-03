8da6db257a81117b2cff8fc49fdf054bc439aa4c
khalilBahi <7753@holbertonstudents.com>
2024-12-03 18:39:44 +0100

    adds two matrices element-wise

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -35,6 +35,10 @@ def add_matrices2D(mat1, mat2):
 
     """
     # Check if matrices have the same dimensions
-    if len(mat1) != len(mat2) or any(len(row1) != len(row2)
-                                     for row1, row2 in zip(mat1, mat2)):
+    if len(mat1) != len(mat2) or any(len(row1) != len(
+            row2) for row1, row2 in zip(mat1, mat2)):
         return None
+
+    result = [[val1 + val2 for val1, val2 in zip(
+        row1, row2)] for row1, row2 in zip(mat1, mat2)]
+    return result
