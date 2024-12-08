32093ef3dde73e1a109019435663a5ee8debb632
khalilBahi <7753@holbertonstudents.com>
2024-12-08 18:11:59 +0100

    performs matrix multiplication

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Perform matrix multiplication of two numpy arrays.
+
+    Args:
+        mat1 (numpy.ndarray): The first matrix.
+        mat2 (numpy.ndarray): The second matrix.
+
+    Returns:
+        numpy.ndarray: The result of matrix multiplication.
+    """
+    return np.matmul(mat1, mat2)
