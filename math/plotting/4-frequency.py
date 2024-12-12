1222dec4a391dddea0a3a69db43a1aeb813aa6d8
khalilBahi <7753@holbertonstudents.com>
2024-12-12 13:13:20 +0100

    plot a histogram of student scores for a project

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -19,11 +19,11 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    bins=np.arange(0, 110, 10)  # Bins every 10 units from 0 to 100
-    plt.hist(student_grades, bins=bins, edgecolor="black")
+    plt.hist(student_grades, bins=np.arange(0, 110, 10), edgecolor="black")
 
     plt.xlim(0, 100)
     plt.ylim((0, 30))
+    plt.xticks(np.arange(0, 110, 10))
 
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
