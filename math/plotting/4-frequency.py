a7740c6236fe0cc7acd5d7d6110a21a0ae0b937b
khalilBahi <7753@holbertonstudents.com>
2024-12-12 12:49:47 +0100

    plot a histogram of student scores for a project

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Task 4: 4. Frequency """
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """
+    plot a histogram of student scores for a project.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+
+    bins = range(0, 101, 10)  # Bins every 10 units from 0 to 100
+    plt.hist(student_grades, bins=bins, edgecolor="black")
+
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+
+    # Show the plot
+    plt.show()
