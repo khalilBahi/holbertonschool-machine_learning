b828c51152312c1bcbf0a9ab93a396b776443c9d
khalilBahi <7753@holbertonstudents.com>
2024-12-20 11:42:59 +0100

    djust the prefix functions to match the desired output format

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -17,9 +17,9 @@ def left_child_add_prefix(text):
         The text with the left child prefix added to each line.
     """
     lines = text.split("\n")
-    new_text = "    +---> " + lines[0] + "\n"
+    new_text = "    +--" + lines[0] + "\n"
     for x in lines[1:]:
-        new_text += ("    |      " + x) + "\n"
+        new_text += ("    |  "+x) + "\n"
     return new_text
 
 
@@ -36,9 +36,9 @@ def right_child_add_prefix(text):
         The text with the right child prefix added to each line.
     """
     lines = text.split("\n")
-    new_text = "    +---> " + lines[0] + "\n"        
+    new_text = "    +--" + lines[0] + "\n"
     for x in lines[1:]:
-        new_text += ("           " + x) + "\n"
+        new_text += ("       " + x) + "\n"
     return new_text
 
 class Node:
