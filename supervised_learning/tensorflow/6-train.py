d4d85a79a2f020e6f919f7edfca1b3eae0dfb29c
khalilBahi <7753@holbertonstudents.com>
2025-01-13 15:22:54 +0100

    Builds, trains, and saves a neural network classifier

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -2,6 +2,8 @@
 """ Task 6: 6. Train """
 import tensorflow.compat.v1 as tf  # type: ignore
 tf.disable_eager_execution()
+import numpy as np
+
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
 create_placeholders = __import__('0-create_placeholders').create_placeholders
