aeef7abdd0e1ddacdfb1e00d109e7305f35c2f09
khalilBahi <7753@holbertonstudents.com>
2025-03-30 21:13:44 +0200

    alculate the likelihood of observing x successes in n trials given probabilities P

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -2,6 +2,7 @@
 """0. Likelihood"""
 import numpy as np
 
+
 def n_choose_x(n, x):
     """
     Computes the binomial coefficient (n choose x).
@@ -20,38 +21,41 @@ def n_choose_x(n, x):
     nx_fact = np.math.factorial(n - x)
     return n_fact / (x_fact * nx_fact)
 
+
 def likelihood(x, n, P):
     """
-    Calculate the likelihood of observing x successes in n trials given probabilities P.
-    
+    Calculate the likelihood of observing x
+    successes in n trials given probabilities P.
+
     Args:
         x: Number of patients with severe side effects
         n: Total number of patients
         P: 1D numpy.ndarray of hypothetical probabilities
-        
+
     Returns:
         1D numpy.ndarray of likelihoods for each probability in P
     """
     # Check if n is a positive integer
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
-        
+
     # Check if x is a non-negative integer
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
-        
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0")
+
     # Check if x is not greater than n
     if x > n:
         raise ValueError("x cannot be greater than n")
-        
+
     # Check if P is a 1D numpy.ndarray
     if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-        
+
     # Check if all values in P are in [0, 1]
     if np.any(P < 0) or np.any(P > 1):
         raise ValueError("All values in P must be in the range [0, 1]")
-    
+
     binomial_coeff = n_choose_x(n, x)
     success_rate = pow(P, x)
     failure_rate = pow(1 - P, n - x)
