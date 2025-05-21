bd1eeab24c85661cf4a5ee00caa33db51dec9cac
khalilBahi <7753@holbertonstudents.com>
2025-05-21 12:37:32 +0200

    creates, builds and trains a gensim word2vec model

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """2. Train Word2Vec"""
-from gensim.models import Word2Vec
+
+import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
@@ -30,7 +31,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     sg = 0 if cbow else 1
 
     # Create and train the Word2Vec model
-    model = Word2Vec(
+    model = gensim.models.Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
