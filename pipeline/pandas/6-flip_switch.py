e466f40947387fef406d3cb29310c201f7936c7c
khalilBahi <7753@holbertonstudents.com>
2025-07-24 12:43:42 +0200

    Module that contains a function to flip and switch a DataFrame

---
+++ pipeline/pandas/6-flip_switch.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function to flip and switch a DataFrame
+"""
+
+
+def flip_switch(df):
+    """
+    Takes a pd.DataFrame and:
+    - Sorts the data in reverse chronological order
+    - Transposes the sorted dataframe
+    
+    Args:
+        df: pd.DataFrame to transform
+        
+    Returns:
+        pd.DataFrame: the transformed DataFrame
+    """
+    # Sort the data in reverse chronological order (descending by index)
+    # Since the data is already in chronological order, we reverse it
+    df_sorted = df.sort_index(ascending=False)
+    
+    # Transpose the sorted dataframe
+    df_transposed = df_sorted.T
+    
+    return df_transposed
