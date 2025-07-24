3616ea3dd97ccfea8ab3d634f1cdd4659654de03
khalilBahi <7753@holbertonstudents.com>
2025-07-24 12:44:12 +0200

    Module that contains a function to sort a DataFrame by High price

---
+++ pipeline/pandas/7-high.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function to sort a DataFrame by High price
+"""
+
+
+
+def high(df):
+    """
+    Takes a pd.DataFrame and:
+    - Sorts it by the High price in descending order
+    
+    Args:
+        df: pd.DataFrame to sort
+        
+    Returns:
+        pd.DataFrame: the sorted DataFrame
+    """
+    # Sort by the High column in descending order
+    df_sorted = df.sort_values(by='High', ascending=False)
+    
+    return df_sorted
