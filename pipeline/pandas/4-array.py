14e7e3ca98d8b182d6ce1c8ac9d8a587a2c29af5
khalilBahi <7753@holbertonstudents.com>
2025-07-24 12:40:48 +0200

    Module that contains a function to convert DataFrame columns to numpy array

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function to convert DataFrame columns to numpy array
+"""
+
+import numpy as np
+
+
+def array(df):
+    """
+    Takes a pd.DataFrame as input and performs the following:
+    - Selects the last 10 rows of the High and Close columns
+    - Converts these selected values into a numpy.ndarray
+
+    Args:
+        df: pd.DataFrame containing columns named High and Close
+
+    Returns:
+        numpy.ndarray: the last 10 rows of High and Close columns
+    """
+    # Select the last 10 rows of High and Close columns
+    last_10_rows = df[["High", "Close"]].tail(10)
+
+    # Convert to numpy array
+    return last_10_rows.to_numpy()
