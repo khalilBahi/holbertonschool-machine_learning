5fed61d60c2182409ea4e1d94582814b672ac32f
khalilBahi <7753@holbertonstudents.com>
2025-10-04 12:03:33 +0100

    tests for the optimum number of clusters by variance

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """3. Optimize k"""
 
-import numpy as np
-kmeans = __import__("1-kmeans").kmeans
-variance = __import__("2-variance").variance
+
 
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
