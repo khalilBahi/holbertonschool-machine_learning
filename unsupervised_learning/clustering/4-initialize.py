065ebfc32496e1f31e20b7c38109a59caca9628e
khalilBahi <7753@holbertonstudents.com>
2025-10-04 12:25:22 +0100

    initializes variables for a Gaussian Mixture Model

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -1,33 +1,40 @@
 #!/usr/bin/env python3
-"""Initializes variables for a Gaussian mixture model"""
+"""Initialize variables for a Gaussian Mixture Model"""
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 
 
 def initialize(X, k):
-    """Initializes variables for a Gaussian mixture model
-    @X: np.ndarray shape(n, d) - data set
-    @k: pos int, number of clusters
-    Returns: pi, m, S or None, None, None on failure
-        @pi: np.ndarray shape(k,) containing the priors for each cluster
-        , initialized evenly
-        @m: np.ndarray shape(k, d) - the centroid means for each cluster,
-        initialized with K-means
-        @S: np.ndarray of shpae(k, d, d) - covariance matrices for each
-        cluster, initialized as identity matrices
     """
+    Initializes variables for a Gaussian Mixture Model.
 
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return (None, None, None)
+    Parameters:
+    X (numpy.ndarray): 2D numpy array of shape (n, d) containing the dataset.
+    k (int): A positive integer containing the number of clusters.
 
+    Returns:
+    tuple: (pi, m, S), or (None, None, None) on failure.
+        - pi is a numpy.ndarray of shape (k,) containing the priors for each
+        cluster, initialized evenly.
+        - m is a numpy.ndarray of shape (k, d) containing the centroid means
+        for each cluster, initialized with K-means.
+        - S is a numpy.ndarray of shape (k, d, d) containing the covariance
+        matrices for each cluster, initialized as identity matrices.
+    """
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None, None, None
     if not isinstance(k, int) or k <= 0:
-        return (None, None, None)
+        return None, None, None
 
-    centroids = kmeans(X, k)[0]
+    # Initialize equal prior probabilities with each cluster
+    pi = np.full((k,), fill_value=1/k)
 
-    pi = np.ones(k) / k
+    # m (centroids) is initialized using K-means
+    m, _ = kmeans(X, k)
+    if m is None:
+        return None, None, None
 
-    d = X.shape[1]
-    S = (np.tile(np.identity(d)[None, :], k)).reshape(k, d, d)
+    # S is initialized with identity matrices
+    S = np.tile(np.eye(X.shape[1]), (k, 1, 1))
 
-    return pi, centroids, S
+    return pi, m, S
