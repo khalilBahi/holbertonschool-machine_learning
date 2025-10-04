091c11dc933db98f30a38d98032991ef4a4388d9
khalilBahi <7753@holbertonstudents.com>
2025-10-04 12:17:06 +0100

    calculates the maximization step in the EM algorithm for a GMM

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -1,35 +1,27 @@
 #!/usr/bin/env python3
-"""
-7. Maximization
-"""
-
-
+"""calculates the maximization step in the EM algorithm for a GMM"""
 import numpy as np
 
 
 def maximization(X, g):
+    """calculates the maximization step in the EM algorithm for a GMM
+    @X: np.ndarray shape(n, d) data set
+    @g: np.ndarray shape(k, n) posterior probs for each data point in clusters
+    Returns: pi, m, S or None, None, None on failure
+        @pi: np.ndarray shape(k,) updated priors for each cluster)
+        @m: np.ndaray shape(k, d) updated centroid means for each cluster
+        @S: np.ndarray of shape(k, d, d) updated covariance matrices for
+        each cluster
     """
-    Calculates the maximization step in the EM algorithm for a GMM
-
-    parameters:
-        X [numpy.ndarray of shape (n, d)]:
-            contains the dataset
-            n: the number of data points
-            d: the number of dimensions for each data point
-        g [numpy.ndarray of shape (k, n)]:
-            containing the posterior probabilities for each data point
-                in the cluster
-
-    should only use one loop
-
-    returns:
-        pi, m, S:
-            pi [numpy.ndarray of shape (k,)]:
-                containing the updated priors for each cluster
-            m [numpy.ndarray of shape (k, d)]:
-                containing the updated centroid means for each cluster
-            S [numpy.ndarray of shape (k, d, d)]:
-                containing the updated covariance matrices for each cluster
-        or None, None, None on failure
-    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None
+    if not isinstance(g, np.ndarray) or len(g.shape) != 2:
+        return None, None, None
+    n, d = X.shape
+    k, ng = g.shape
+    if n != ng:
+        return None, None, None
+    if int(np.sum(np.sum(g, axis=0))) != n:
+        return None, None, None
+    # placer
     return None, None, None
