74bd6cd2d54ff2d2ba49c61e87de84f64a583519
khalilBahi <7753@holbertonstudents.com>
2025-10-09 18:38:13 +0100

    implement evaluate to load from checkpoint, fetch tensors from collections, and compute metrics

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """7: Evaluate"""
 import tensorflow.compat.v1 as tf  # type: ignore
+
 tf.disable_eager_execution()
+
 
 def evaluate(X, Y, save_path):
     """Evaluates a neural network loaded from a TensorFlow checkpoint.
@@ -19,17 +21,16 @@ def evaluate(X, Y, save_path):
     """
     tf.reset_default_graph()
     with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(save_path + '.meta')
+        saver = tf.train.import_meta_graph(save_path + ".meta")
         saver.restore(sess, save_path)
 
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        y_pred = tf.get_collection('y_pred')[0]
-        accuracy = tf.get_collection('accuracy')[0]
-        loss = tf.get_collection('loss')[0]
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
 
         y_pred_val, acc_val, loss_val = sess.run(
-            [y_pred, accuracy, loss],
-            feed_dict={x: X, y: Y}
+            [y_pred, accuracy, loss], feed_dict={x: X, y: Y}
         )
     return y_pred_val, acc_val, loss_val
