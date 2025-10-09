5283ee8f64facc185e162260f15c8b68559e7038
khalilBahi <7753@holbertonstudents.com>
2025-10-09 19:03:42 +0100

    create mini-batches from the shuffled data

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -4,29 +4,28 @@ import numpy as np
 shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
-def create_mini_batches(x_shuffled, y_shuffled, batch_size):
+def create_mini_batches(X, Y, batch_size):
     """
-    Create mini-batches from already shuffled data.
+    Create mini-batches from input data.
 
     Args:
-        x_shuffled (numpy.ndarray): Shuffled input data of shape (m, nx).
-        y_shuffled (numpy.ndarray): Shuffled labels of shape (m, ny).
+        X (numpy.ndarray): Input data of shape (m, nx).
+        Y (numpy.ndarray): Labels of shape (m, ny).
         batch_size (int): Number of data points in a mini-batch.
 
     Returns:
         list: A list of mini-batches, where each mini-batch is a tuple (X_batch, Y_batch).
     """
-    m = x_shuffled.shape[0]  # Number of data points
+    # First shuffle the data
+    X_shuffled, Y_shuffled = shuffle_data(X, Y)
+    
+    m = X_shuffled.shape[0]  # Number of data points
     mini_batches = []
-
-    # If batch_size > m, set batch_size = m
-    if batch_size > m:
-        batch_size = m
 
     # Create mini-batches
     for i in range(0, m, batch_size):
-        X_batch = x_shuffled[i:i + batch_size]
-        Y_batch = y_shuffled[i:i + batch_size]
+        X_batch = X_shuffled[i:i + batch_size]
+        Y_batch = Y_shuffled[i:i + batch_size]
         mini_batches.append((X_batch, Y_batch))
 
     return mini_batches
